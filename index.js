a697b5132453f2facb568d879af3046ff27de89c
Abhas Bhattacharya <abhasbhattacharya2@gmail.com>
2015-09-22 03:13:54 +0530

    supporting number index in both and var index in simplesyntax only

--- index.js
+++ index.js
@@ -1,12 +1,58 @@
 var postcss = require('postcss');
 
+
+function replaceRight(str_whole, regexp_to_match, func_to_replace){
+    var index_end = str_whole.length
+    var arr_last_match = null
+    while(true)
+    {
+        arr_last_match = null
+        arr_next_match = null
+        var index_start = 0
+        while (true)
+        {
+            str_part = str_whole.substring(index_start,index_end)
+            // console.log(str_part)
+            arr_next_match = regexp_to_match.exec(str_part)
+
+            // console.log(arr_next_match)
+            if (arr_next_match === null)
+            {
+                // console.log("breaking")
+                break;
+            }
+            else
+            {
+                arr_last_match = arr_next_match
+                arr_last_match.index_start = index_start
+                index_start = index_start + arr_last_match[0].length
+            }
+        }
+        if (arr_last_match === null)
+        {
+            return str_whole;
+        }
+        else
+        {
+            // console.log(arr_last_match, arr_last_match.index_start)
+            index_end = arr_last_match.index_start + arr_last_match.index
+            extra_string_info= {"str":str_whole, "match_start":index_start,"match_end":index_end, "type":"extra_string_info"}
+            replaced_str = func_to_replace.apply(null, [].concat(arr_last_match).concat([arr_last_match.index,arr_last_match.input,extra_string_info]))
+            str_whole = str_whole.substring(0, index_end ) + replaced_str + str_whole.substring(index_end + arr_last_match[0].length)
+            // console.log(str_whole)
+        }
+    }
+}
+
+
 var definition = function (variables, node) {
     var name = node.prop.slice(1);
     variables[name] = node.value;
+    // console.log(node.value)
     node.remove();
 };
 
-var variable = function (variables, node, str, name, opts, result) {
+var variable = function (variables, node, str, name, index, opts, result) {
     if ( opts.only ) {
         if ( typeof opts.only[name] !== 'undefined' ) {
             return opts.only[name];
@@ -15,7 +61,19 @@ var variable = function (variables, node, str, name, opts, result) {
         }
 
     } if ( typeof variables[name] !== 'undefined' ) {
-        return variables[name];
+        var tmp_var = variables[name]
+        // console.log(tmp_var, typeof(tmp_var), index, typeof(index))
+        if (index === undefined)
+        {
+            return tmp_var;
+        }
+        else
+        {
+            index = Number(index);
+            console.log(index);
+            tmp_var = JSON.parse(tmp_var)
+            return tmp_var[index];
+        }
 
     } else if ( opts.silent ) {
         return str;
@@ -31,14 +89,34 @@ var variable = function (variables, node, str, name, opts, result) {
 };
 
 var simpleSyntax = function (variables, node, str, opts, result) {
-    return str.replace(/(^|[^\w])\$([\w\d-_]+)/g, function (_, bef, name) {
-        return bef + variable(variables, node, '$' + name, name, opts, result);
+    return replaceRight(str,/\$([\w\d-_]+)(?:\[(\d+)\])?/, function (_, name, index) {
+        console.log("simple", name)
+        // console.log(index)
+
+        /* catch case of $arr[$i], as ori replaceright doesnt catch $i in str to match */
+        var extra_string_info = arguments[arguments.length - 1]
+        console.assert(extra_string_info.type == "extra_string_info")
+        var str_whole = extra_string_info.str
+        var str_whole_length = str_whole.length
+        var str_unmatched_right = str_whole.substring(extra_string_info.index_end, str_whole_length)
+        console.log(str_unmatched_right)
+        var regexp_index_only = /^\[(\d+)\]/
+        var matches_index = regexp_index_only.exec(str_unmatched_right)
+        if (matches_index !== null)
+        {
+            index = matches_index[1]
+            console.log(index)
+        }
+
+        return variable(variables, node, '$' + name, name, index, opts, result);
     });
 };
 
 var inStringSyntax = function (variables, node, str, opts, result) {
-    return str.replace(/\$\(\s*([\w\d-_]+)\s*\)/g, function (all, name) {
-        return variable(variables, node, all, name, opts, result);
+    return replaceRight(str,/\$\(\s*([\w\d-_]+)(?:\[(\d+)\])?\s*\)/, function (all, name, index) {
+        // console.log("in", all, name)
+        // console.log(index)
+        return variable(variables, node, all, name, index, opts, result);
     });
 };
 
